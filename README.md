45633a5a744795e48b0348f951cd6a92c39a21ef
xuanntVnext <xuannt@vnext.vn>
2017-01-25 18:10:13 +0700

    Release phase 1.

---
+++ README.md
@@ -0,0 +1,39 @@
+jQuery indicator
+==========
+
+Easy jQuery plugin to show visual feedback when loading data or any action that would take time. Also can disable form elements and put an overlay an specified area or whole page.
+
+__Installation__
+
+2 options:
+
+**use Bower**
+```bash
+bower install llama-jquery-indicator
+```
+
+**use NPM**
+```bash
+npm install llama-jquery-indicator --save-dev
+```
+
+__Quick start__
+
+Basic use:
+```javascript
+$( "selector" ).indicator("show");
+```
+
+Stop:
+```javascript
+$( "selector" ).indicator( "hide" );
+```
+
+Default options:
+
+```javascript
+defaults = {
+    'appendTo': "body",        // Place for element indicator will be append
+    'html': null               // Element indicator code
+};
+```
---
+++ src/jquery.indicator.js
@@ -0,0 +1,109 @@
+(function(factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory(require('jquery'));
+    } else {
+        factory(jQuery);
+    }
+}(function($, undefined) {
+    var counter = {};
+
+    // The actual plugin constructor
+    $.indicator = function(element, options) {
+        this.$el = $(element);
+        this.$opts = options;
+        this.init();
+    }
+
+    $.extend($.indicator, {
+        defaults: {
+            appendTo: 'body',
+            html: null
+        },
+
+        setDefaults: function(options) {
+            $.extend(this.defaults, options);
+        },
+
+        prototype: {
+
+            init: function() {
+                // Determine whether indicator already exists yet.
+                if (this.$el.length === 0) {
+                    this.$el = $(this.$opts.html);
+
+                    if (this.$el.length === 0) {
+                        throw new Error('Unknown element indicator');
+                    }
+
+                    this.$el.appendTo(this.$opts.appendTo);
+                }
+
+                // Sets counter
+                counter[this.$el[0]] = 0;
+
+                // Always hide indicator for first time
+                this.hide();
+            },
+
+            show: function() {
+                counter[this.$el[0]]++;
+                this.$el.show();
+            },
+
+            hide: function() {
+                if (counter[this.$el[0]] > 0) {
+                    counter[this.$el[0]]--;
+                }
+
+                // The indicator just hidden when count decrement to 0
+                if (counter[this.$el[0]] === 0) {
+                    this.$el.hide();
+                }
+            }
+        }
+    });
+
+    // PLUGIN DEFINITION
+    // =======================
+    var allowedMethods = [
+        'show', 'hide',
+        'destroy'
+    ];
+
+    // Constructor
+    $.fn.indicator = function(option) {
+        var $this;
+        if (this[0] === undefined) {
+            $this = $(document);
+        } else {
+            $this = $(this);
+        }
+
+        var args = Array.prototype.slice.call(arguments, 1);
+        var pluginName = 'jquery.indicator';
+        var data = $this.data(pluginName);
+        var options = $.extend({}, $.indicator.defaults, $this.data(), typeof option === 'object' && option);
+
+        if (!data) {
+            $this.data(pluginName, (data = new $.indicator(this, options)));
+        }
+
+        if (typeof option === 'string') {
+            if ($.inArray(option, allowedMethods) < 0) {
+                throw new Error('Unknown method: ' + option);
+            }
+
+            if (option === 'destroy') {
+                $this.removeData(pluginName);
+            } else {
+                data[option].apply(data, args);
+            }
+        }
+
+        return data;
+    }
+
+    $.fn.indicator.Constructor = $.indicator;
+}));
